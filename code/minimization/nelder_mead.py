3f0299875f8e7b84348e825f06a4d48fe091eef0
Rotem Gazit <gazitrotem@gmail.com>
2020-02-22 18:21:27 +0200

    adding pretty predict and minimization

---
+++ code/minimization/nelder_mead.py
@@ -0,0 +1,49 @@
+import random
+
+import numpy as np
+
+ALPHA, GAMA, RO, SIGMA = 1., 2., 0.5, 0.5
+
+
+def nelder_mead_terminate(iteration, iteration_limit, x, error=1e-3):
+    if iteration >= iteration_limit:
+        return True
+    for i in range(len(x) - 1):
+        if np.linalg.norm(x[i] - x[i + 1]) > error:
+            return False
+    return True
+
+
+def nelder_mead_minimize_(f, n, xinit, iteration, error=1e-3, iteration_limit=100):
+    xinit = sorted(xinit, key=f)
+    fx = [f(x) for x in xinit]
+    x0 = sum(xinit[:n]) / n
+    if nelder_mead_terminate(iteration, iteration_limit, xinit, error):
+        return x0
+    else:
+        xr = x0 + ALPHA * (x0 - xinit[n])
+        if fx[0] <= f(xr) < fx[n]:
+            return nelder_mead_minimize_(f, n, xinit[:n] + [xr], iteration + 1, error)
+        if f(xr) < fx[0]:
+            xe = x0 + GAMA * (xr - x0)
+            if f(xe) < f(xr):
+                return nelder_mead_minimize_(f, n, xinit[:n] + [xe], iteration + 1, error)
+            else:
+                return nelder_mead_minimize_(f, n, xinit[:n] + [xr], iteration + 1, error)
+        else:
+            xc = x0 + RO * (xinit[n] - x0)
+            if f(xc) < fx[n]:
+                return nelder_mead_minimize_(f, n, xinit[:n] + [xc], iteration + 1, error)
+        shrink_xinit = [xinit[0]] + [xinit[0] + SIGMA * (x - xinit[0]) for x in xinit[1:]]
+        return nelder_mead_minimize_(f, n, shrink_xinit, iteration + 1, error)
+
+
+def nelder_mead_minimize(f, x0, error=1e-3, iteration_limit=100):
+    n = len(x0)
+    xinit = [x0]
+    for i in range(n):
+        x = x0.copy()
+        r = random.uniform(- 10 * error, 10 * error)
+        x[i] = float(x[i]) + r
+        xinit.append(x)
+    return nelder_mead_minimize_(f, n, xinit, 0, error, iteration_limit)
---
+++ code/model/pretty_predict.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from IPython.core.display import display
+
+
+def plot_day_with_predict_func(df, model, predict):
+    gen = df.sample(n=1, random_state=42)
+    display(gen)
+    gen = gen.values[0]
+    x, y = [], []
+    for i in range(0, 60 * 60 * 24, 100):
+        x.append(i / 60 / 60)
+        y.append(predict(df, model, gen, i))
+    plt.plot(x, y)
+    plt.show()
+    return gen
+
+
+def predict(df, model, row, time_of_day=None, buffer=1000, buffer_samples=21):
+    time_of_day_index = list(df.columns).index('time_of_day')
+    rows = []
+    time_of_day = time_of_day or row[time_of_day_index]
+    for i in np.linspace(max(time_of_day - buffer, 0), min(time_of_day + buffer, 60 * 60 * 24), buffer_samples):
+        row = row.copy()
+        row[time_of_day_index] = i
+        rows.append(row)
+    rows = pd.DataFrame(rows, columns=df.columns)
+    preds = model.predict(rows)
+    return preds.mean()
