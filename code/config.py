ec00afa0fb32492cf3d026a3462cdafbbbab8b4e
Rotem Gazit <gazitrotem@gmail.com>
2020-02-22 17:20:11 +0200

    environment dev

--- code/config.py
+++ code/config.py
@@ -1,7 +1,12 @@
+ENVIRONMENT = 'dev'  ## change to prod to full data processing
 PATH = './data/'
 
-TRIP_DATA_PATH = PATH + 'trip_data'
+TRIP_DATA_PATH = PATH + 'trip_data/trip_data_1.csv'
+TRIP_DATA_SAMPLE_PATH = PATH + 'trip_data_sample'
 TRIP_DATA_GRIDS_PATH = PATH + 'trip_data_grids'
+
+if ENVIRONMENT != 'prod':
+    TRIP_DATA_PATH = TRIP_DATA_SAMPLE_PATH
 
 PICKUP_COLUMN = 'pickup_grid'
 DROPOFF_COLUMN = 'dropoff_grid'
