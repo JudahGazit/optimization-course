6ddba871633d744c4ce7a4173d829c9e5872093d
Rotem Gazit <gazitrotem@gmail.com>
2020-03-13 22:24:19 +0200

    altering stats file

--- python_code/raw_data_stats.py
+++ python_code/raw_data_stats.py
@@ -9,7 +9,7 @@ from python_code.config import TRIP_DATA_GRIDS_PATH
 def load_data(pickup_grid='87G8Q279+', dropoff_grid='87G8Q225+'):
     data_path = os.path.join(os.path.abspath(TRIP_DATA_GRIDS_PATH), f'trip_data_{pickup_grid}_{dropoff_grid}.csv')
     df = pd.read_csv(data_path)
-    df = df[(df.trip_time_in_secs > 0) & (df.trip_distance > 0.05) & (df.trip_distance < 2)]
+    df = df[(df.trip_time_in_secs > 0) & (df.trip_distance > 0.65) & (df.trip_distance < 2)]
     return df
 
 
@@ -59,7 +59,7 @@ def run_and_save_png(png_path, func, *args, **kwargs):
 
 if __name__ == '__main__':
     df = load_data()
-    graphs = [trip_time_hist, time_of_day_hist, trip_speed_hist, trip_distance_hist]
+    graphs = [trip_distance_hist]
     png_output_path = os.path.abspath('./latex')
     for graph in graphs:
         run_and_save_png(png_output_path, graph, df)
