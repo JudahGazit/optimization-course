328efdb9fbc28aecafe74e489ed61644d99912c7
Rotem Gazit <gazitrotem@gmail.com>
2020-03-13 19:56:54 +0200

    adding means to weather

--- python_code/model/features/weather.py
+++ python_code/model/features/weather.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from python_code.config import PATH
@@ -9,8 +10,24 @@ def load_weather_file(file_name):
     weather.columns = ['datetime', file_name]
     weather.datetime = pd.to_datetime(weather.datetime, utc=True)
     weather.datetime = weather.datetime.dt.tz_convert('US/Eastern')
+    weather = calculate_daily_mean(weather, file_name)
     weather.datetime = weather.datetime.dt.strftime('%Y-%m-%d %H:00:00')
     return weather
+
+
+def calculate_daily_mean(weather_dataset, file_name, date_column='DATE'):
+    if weather_dataset.dtypes[file_name] == np.float:
+        weather_dataset[date_column] = weather_dataset.datetime.dt.strftime('%Y-%m-%d')
+        mean_column, min_column, max_column = f'{file_name}_daily_mean', f'{file_name}_daily_min', f'{file_name}_daily_max'
+        means = weather_dataset[[date_column, file_name]]
+        means[mean_column] = means[file_name]
+        means[min_column] = means[file_name]
+        means[max_column] = means[file_name]
+        means = means.groupby([date_column], as_index=False).agg(
+            {mean_column: 'mean', min_column: 'min', max_column: 'max'})
+        means = means[[date_column, mean_column, min_column, max_column]]
+        weather_dataset = weather_dataset.merge(means, on=[date_column]).drop([date_column], 1)
+    return weather_dataset
 
 
 def add_weather(df):
